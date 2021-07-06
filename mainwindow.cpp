2ab6fe42412a702a18659b3b394841a5e89400e2
mariam <mariammohammad390@gmail.com>
2021-07-06 12:08:14 +0200

    open file button

---
+++ mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+QFile mytempfile("out.txt");
+QFile myfile("myfile.txt");
+void MainWindow::on_OpenFileButton_clicked()
+{
+    ui->input_text->clear();
+    QFile input_file(QFileDialog::getOpenFileName(this,tr("Open File"),"",tr("XML File (*.xml) ;;TextFile (*.txt)")));
+    input_file.open(QIODevice::ReadOnly |QIODevice::Text);
+    QTextStream stream(&input_file);
+    QString text= stream.readAll();
+    myfile.remove();
+    mytempfile.resize(0);
+    input_file.copy("myfile.txt");
+    QFile myfile("myfile.txt");
+    ui->input_text->setPlainText(text);
+    ui->input_text->setLineWrapMode(QPlainTextEdit::NoWrap);
+   input_file.close();
+}

28336ead4b9ecfcf79d62de700d49afb680c7932
Mariam Fahmy <mariamfahmy66@gmail.com>
2021-07-11 23:44:38 +0200

    XML-Editor, removing unnecessary comments.

--- Core/tag-block.cpp
+++ Core/tag-block.cpp
@@ -3,7 +3,6 @@
 TagBlock::TagBlock(QString content, TagType type)
     : MainBlock(content, type)
     , internalBlocks(new QVector<MainBlock*>())
-    //, tagAttributes(new QMap<QString, QString> ())
 {}
 
 TagBlock::~TagBlock()
@@ -14,15 +13,9 @@ TagBlock::~TagBlock()
         delete (*internalBlocks)[i];
     }
     delete internalBlocks;
-    //delete tagAttributes;
 }
 
 QVector<MainBlock *> * TagBlock::getInternalBlocks()
 {
     return internalBlocks;
 }
-
-/*QMap<QString, QString> * TagBlock::getTagAttributes()
-{
-    return tagAttributes;
-}*/
--- Core/tag-block.h
+++ Core/tag-block.h
@@ -11,13 +11,11 @@ class TagBlock : public MainBlock
 private:
     //internalBlocks is a pointer to a QVector, and each element in the QVector is a pointer to a MainBlock object.
     QVector<MainBlock *> * internalBlocks;
-    //QMap<QString, QString> * tagAttributes;
 public:
     TagBlock(QString content, TagType tagType);
     ~TagBlock() override;
 
     QVector<MainBlock *> * getInternalBlocks() override;
-    //QMap<QString, QString> * getTagAttributes() override;
 };
 
 #endif // TAGBLOCK_H
